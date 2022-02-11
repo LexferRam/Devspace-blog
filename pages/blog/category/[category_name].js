71e3e5980012d4e43621e8e2c8992e38e9f4e709
Lexfer <lexferramirez@gmail.com>
2022-02-10 21:58:37 -0400

    [ADD] CategoryBlogPage

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -6,10 +6,10 @@ import Layout from '@/components/Layout'
 import Post from '@/components/Post'
 import {sortByDate} from '@/utils/index'
 
-export default function CategoryBlogPage({posts}) {
+export default function CategoryBlogPage({posts,category_name}) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Post</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Post in {category_name}</h1>
 
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {posts.map((post,index) => (
@@ -17,20 +17,33 @@ export default function CategoryBlogPage({posts}) {
         ))}
       </div>
 
-      <Link href='/blog'>
-        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transitions duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
-          All Posts
-        </a>
-      </Link>
     </Layout>
   )
 }
 
-// export async function getstaticPaths(){
-//     cosnt files = fs.readdirSync(path.join('posts'))
-// }
+export async function getStaticPaths(){
+    const files = fs.readdirSync(path.join('posts'))
 
-export async function getStaticProps(){
+    const categories = files.map(filename => {
+        const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+
+        const {data:frontmatter} = matter(markdownWithMeta)
+
+        return frontmatter.category.toLowerCase()
+    })
+
+    const paths = categories.map(category => ({
+        params:{category_name: category}
+    }))
+
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({params:{category_name}}){
+
   const files = fs.readdirSync(path.join('posts'))
 
   const posts = files.map(filename => {
@@ -43,10 +56,13 @@ export async function getStaticProps(){
     return{slug,frontmatter}
   })
 
+  //filter post by category_name
+  const categoryPosts = posts.filter(post => post.frontmatter.category.toLowerCase() === category_name)
 
   return {
     props:{
-      posts :posts.sort(sortByDate).slice(0,6)
+      posts :categoryPosts.sort(sortByDate), 
+      categoryName: category_name,
     }
   }
 }
